c2615fd36825fbf497113785c343756557d8b97c
mitc0380 <mitc0380@morris.umn.edu>
2016-09-22 15:39:12 -0500

    prints the keys of the hashmap

--- Lab5/src/Matcher.java
+++ Lab5/src/Matcher.java
@@ -1,4 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 public class Matcher {
 
+	public static HashMap<String,ArrayList<String>> generateHash(int n){
+		HashMap<String,ArrayList<String>> linkedIn = new HashMap(n);
+		for (int i=1; i<=n;i++){
+			ArrayList<String> temp = new ArrayList<String>(n);
+			for (int j=1;j<=n;j++){
+				String tempS = "C"+j;
+				temp.add(tempS);
+			}
+			Collections.shuffle(temp);
+			linkedIn.put("P" + i, temp);
+		}
+		for (int i=1; i<=n;i++){
+			ArrayList<String> temp = new ArrayList<String>(n);
+			for (int j=1;j<=n;j++){
+				String tempS = "P"+j;
+				temp.add(tempS);
+			}
+			Collections.shuffle(temp);
+			linkedIn.put("C" + i, temp);
+		}
+		return linkedIn;
+	}
+
+	public static void printHashMap(HashMap<String,ArrayList<String>> hash){
+		int numOfPairs = hash.size() / 2;
+		ArrayList listOfKeys = new ArrayList();
+		for (String key : hash.keySet()){
+			listOfKeys.add(key);
+		}
+		Collections.sort(listOfKeys);
+		System.out.println(Arrays.toString(listOfKeys.toArray()));
+		
+	}
+
+	public static void main(String [] args){
+		printHashMap(generateHash(5));
+	}
 }
