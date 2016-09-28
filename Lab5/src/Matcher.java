893bb9f1fe03ec40957d539d1ae2f81408adf0ed
mitc0380 <mitc0380@morris.umn.edu>
2016-09-28 16:39:08 -0500

    made testing file and put all methods that don't affect algoritm inside, added question responses to git

--- Lab5/src/Matcher.java
+++ Lab5/src/Matcher.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-
 import java.util.LinkedHashMap;
 
 
@@ -13,7 +11,7 @@ public class Matcher {
 	public static LinkedHashMap<String,ArrayList<String>> linkedInP = new LinkedHashMap<String,ArrayList<String>>();
 	public static LinkedHashMap<String,ArrayList<String>> linkedInC = new LinkedHashMap<String,ArrayList<String>>();
 
-	//generates initial hashmap of n companies and programmers
+	//generates initial random hashmap of n companies and programmers
 	public static void generateHash(int n){
 
 		numOfPairs = n;
@@ -39,39 +37,11 @@ public class Matcher {
 
 	}
 	
-	//method for printing the initially generated hashmap
-	public static void printHashMap(){
-		ArrayList<String> listOfKeys = new ArrayList<String>();
-
-		for (String key : linkedInP.keySet()){
-			ArrayList<String> temp = linkedInP.get(key);
-			System.out.print(key + " ");
-			System.out.println(Arrays.toString(temp.toArray()));
-			listOfKeys.add(key);
-		}
-		System.out.println("*************************************");
-		for (String key : linkedInC.keySet()){
-			ArrayList<String> temp = linkedInC.get(key);
-			System.out.print(key + " ");
-			System.out.println(Arrays.toString(temp.toArray()));
-			listOfKeys.add(key);
-		}
-		System.out.println("*************************************");
-	}
-	
 	//sets initial preferences- not satisfactory
 	public static void generateStartState(){
 		for (int i = 1;i<=numOfPairs;i++){
 			currentState.put("C" + i, "P" + i);
 		}
-	}
-	
-	//prints the pairs of companies and programmers
-	public static void printState(){
-		for (int i = 1; i <= numOfPairs; i++){
-			System.out.println("C" + i + " " + currentState.get("C" + i));
-		}
-		System.out.println("*************************************");
 	}
 	
 	//gets the ranking of a company and programmer pair
@@ -109,19 +79,6 @@ public class Matcher {
 		return false;
 	}
 	
-	//checks if pairing is satisfactory-basically the reverse of canSwap
-	public static boolean isSatisfactory(String C1, String P1, String C2, String P2){
-		int rankP1C1 = getRank(P1,C1);
-		int rankC2P2 = getRank(C2,P2);
-		int rankC2P1 = getRank(C2,P1);
-		int rankP1C2 = getRank(P1,C2);
-		
-		if ((rankP1C2 < rankP1C1) && (rankC2P1 < rankC2P2)){
-			return false;
-		}
-		return true;
-	}
-	
 	//swaps elements
 	public static void doSwap(String C1, String P1, String C2, String P2){
 		currentState.replace(C1, P2);
@@ -151,41 +108,5 @@ public class Matcher {
 		}
 	}
 	
-	//checks all pairs for satisfaction and prints the result
-	public static void checkForSatisfaction(){
-		int counter = 0;
-		
-		for (int i = 1;i <=numOfPairs;i++){
-			String Ci = "C" + i;
-			for (int j = 1; j <= numOfPairs; j++){
-				String Cj = "C" + j;
-				if (isSatisfactory(Ci,currentState.get(Ci),Cj,currentState.get(Cj))){
-					counter++;
-				}
-				if (isSatisfactory(Cj,currentState.get(Cj),Ci,currentState.get(Ci))){
-					counter++;
-				}
-			}
-		}
-		if (counter != 2 * numOfPairs*numOfPairs){
-			System.out.println("There is unsatisfaction");
-		}
-		else 
-			System.out.println("Satisfaction guaranteed");
-	}
 
-	//main
-	public static void main(String [] args){
-		for(int i = 0; i<3; i++){
-			System.out.println("Testing " + (i+5) + " Companies and Programmers");
-			generateHash(i+5);
-			generateStartState();
-			printHashMap();
-			//printState();
-			match();
-			printState();
-			checkForSatisfaction();
-			System.out.println("*************************************");
-		}
-	}
 }
